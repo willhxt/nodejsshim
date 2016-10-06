1aaf36465bd75a9274dcd008e8031de266539b36
Ben Thompson <ben@xtuple.com>
2016-10-06 15:11:57 -0400

    Call createpkgschema() correctly.

--- database/source/package.sql
+++ database/source/package.sql
@@ -1,45 +1,7 @@
-DO $$
-
-  var insertSql = "INSERT INTO pkghead (\n" +
-                  "  pkghead_name,\n" +
-                  "  pkghead_descrip,\n" +
-                  "  pkghead_version,\n" +
-                  "  pkghead_developer,\n" +
-                  "  pkghead_notes\n" +
-                  ") VALUES (\n" +
-                  "  $1,\n" +
-                  "  $2,\n" +
-                  "  $3,\n" +
-                  "  $4,\n" +
-                  "  $5\n" +
-                  ");";
-  var selectSql = "SELECT pkghead_id FROM pkghead WHERE (pkghead_name='nodejsshim');";
-  var updateSql = "UPDATE pkghead SET\n" +
-                  "  pkghead_descrip = $1,\n" +
-                  "  pkghead_version = $2,\n" +
-                  "  pkghead_developer = $3,\n" +
-                  "  pkghead_notes = $4\n" +
-                  "WHERE pkghead_name='nodejsshim';";
-
-  var select = plv8.execute(selectSql);
-
-  if (select.length && select.length === 1) {
-    plv8.execute(updateSql, [
-      "xTuple ERP Node.js shims for the Qt Script Engine.",
-      "1.0.0-rc",
-      "xTuple",
-      "This package creates the required database tables and functions for xTuple ERP Node.js shims for the Qt Script Engine."
-    ]);
-  } else if (select.length === 0) {
-    plv8.execute(insertSql, [
-      "nodejsshim",
-      "xTuple ERP Node.js shims for the Qt Script Engine.",
-      "1.0.0-rc",
-      "xTuple",
-      "This package creates the required database tables and functions for xTuple ERP Node.js shims for the Qt Script Engine."
-    ]);
-  } else {
-    plv8.elog(ERROR, "Error creating or updating nodejsshim record in the pkghead table.");
-  }
-
-$$ language plv8;
+SELECT createpkgschema('nodejsshim',
+                       $PKG$This package creates the required database tables and functions for xTuple Node.js shims.
+This schema is part of the Node.js shims for the Qt Script Engine Package for xTuple ERP, and is Copyright (c) 1999-2016 by OpenMFG LLC, d/b/a xTuple.  It is licensed to you under the xTuple End-User License Agreement ("the EULA"), the full text of which is available at www.xtuple.com/EULA.  While the EULA gives you access to source code and encourages your involvement in the development process, this Package is not free software.  By using this software, you agree to be bound by the terms of the EULA.$PKG$);
+update pkghead set pkghead_descrip = 'xTuple ERP Node.js shims',
+                   pkghead_version = '1.0.0-rc',
+                   pkghead_developer = 'xTuple'
+ where pkghead_name = 'nodejsshim';
