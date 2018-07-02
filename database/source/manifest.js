59065b6d2c469c70467cb6d8b4110854f46922c6
Ben Thompson <ben@xtuple.com>
2018-07-02 13:46:22 -0400

    Bump version number to 1.0.2 for next release cycle.

--- database/source/manifest.js
+++ database/source/manifest.js
@@ -1,6 +1,6 @@
 {
   "name": "nodejsshim",
-  "version": "1.0.0",
+  "version": "1.0.1",
   "comment": "xTuple ERP Node.js shims for the Qt Script Engine.",
   "loadOrder": 10,
   "defaultSchema": "nodejsshim",
